a488c0f9d7579d1de605e0cf569248846f8d3a25
kavitharajsekaran <kavitha.rajasekaran@rapidqube.com>
2020-05-07 21:56:29 +0530

    changed the url from localhost to heroku

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://kavitharajasekaran:Pavankumar2007@ds253857.mlab.com:53857/advance-directive', { useNewUrlParser: true })
-mongoose.connect("mongodb://localhost:27017/node-demo",{useUnifiedTopology: true,useNewUrlParser: true })
+mongoose.connect('mongodb://kavitharajasekaran:Pavankumar2007@ds253857.mlab.com:53857/advance-directive', {useUnifiedTopology: true,useNewUrlParser: true })
+// mongoose.connect("mongodb://localhost:27017/node-demo",{useUnifiedTopology: true,useNewUrlParser: true })
 .then(() => {
     console.log('connected to mongoDB');
 })
